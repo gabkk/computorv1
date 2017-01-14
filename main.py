db2882d98ead5c6c1661e7cb2bead75bf70d1def
gabriel kuma <gkuma@student.42.fr>
2017-01-14 18:54:45 +0100

    issue with 5X^1 et 5X

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-
-
 import re
 import sys
 import string
 
 
 def remove_x(liste): 
-    pattern = r"([^\^][0-9]+)"
+    pattern = r"([\+|\-]?[0-9]*[\.]?[0-9]*)"
     value = []
     for x in liste:
         tmp = re.findall(pattern, x)
         if len(tmp):
-            value.append(tmp[0])
+            if tmp[0]:
+                value.append(tmp[0])
+            else:
+                value.append("1")
         else:
             value.append("1")
     return value
@@ -21,12 +22,6 @@ def remove_value_from_list(the_list, source):
         if val in the_list:
             the_list.remove(val)
 
-def remove_value_and_add_zero(the_list, source):
-    for val in source:
-        if val in the_list:
-            the_list.remove(val)
-            the_list.append("0")
-
 def get_sign(stri):
     pos = re.findall(r"\-", stri)
     if pos:
@@ -34,81 +29,214 @@ def get_sign(stri):
     else:
         return ('+')
 
-def modif_string_to_int(liste):
+def modif_string_to_float(liste):
     new_value = 0
-    tmp = ""
+    tmp = []
     value = 0
+    print "\n TOOOOO \n"
     for x in liste:
-        print "value:"
-        print value
-        print "x:"
-        print x
         pos = get_sign(x)
+        print "x is : "
+        print x
+        print "pos is : "
         print pos
-        tmp = re.findall(r"[0-9]", x)
+        tmp = re.findall(r"([0-9]*[\.]?[0-9]*)", x)
+        print "tmp is : "
         print tmp
         for x in tmp:
-            value = int(x)
-        print value
+            if x:
+                value = float(x)
         if (pos == '-'):
-            value *= -1
+            value *= -1.
+        print "value is : "
+        print value
         new_value += value
+    print "new_value is : "
+    print new_value
     return new_value
 
-def set_list(liste):
+def remove_x_in_num(num):
+    for x in num:
+        is_x = re.findall(r"X",x)
+        if is_x:
+            num.remove(x)
+    return num
+
+def clean_num(num):
+    new_num = []
+    for x in num:
+        x = x.replace(" ", "") 
+        new_num.append(x)
+    return new_num
+
+def parse_value(liste):
     num = []
     x1 = []
     x2 = []
     x1_empty = []
     x_zero = []
     final_list = []
-    pattern = r"([^\^][0-9]+)"
-    pattern1 = r"(.[0-9]*X\^1)"
-    pattern_zero = r"(.[0-9]*X\^0)"
-    pattern1_empty = r"(.[0-9]*X[^\^])"
-    pattern2 = r"(.[0-9]*X\^2)"
+    pattern = r"([^\^][^X][0-9]*[\.]?[0-9]*)"
+    pattern1 = r"([\+|\-]?[[0-9]*[\.]?[0-9]*]*X\^1)"
+    pattern2 = r"([\+|\-]?[[0-9]*[\.]?[0-9]*]*X\^2)"
+    pattern_zero = r"([\+|\-]?[[0-9]*[\.]?[0-9]*]*X\^0)"
+    pattern1_empty = r"([\+|\-]?[[0-9]*[\.]?[0-9]*]*X[^\^])"
+    
     num = re.findall(pattern,liste)
     x1 = re.findall(pattern1,liste)
     x_zero = re.findall(pattern_zero,liste)
     x1_empty = re.findall(pattern1_empty,liste)
     x2 = re.findall(pattern2,liste)
+    
+    print " BEFORE num"
+    print num
+    print "x1"
+    print x1
+    print "x2"
+    print x2
+    
     x1 = remove_x(x1)
     x2 = remove_x(x2)
+
     x1_empty = remove_x(x1_empty)
     x_zero = remove_x(x_zero)
     x_1 = x1 + x1_empty
-    remove_value_and_add_zero(num, x_zero)
+    num = remove_x_in_num(num)
+   
+    num = clean_num(num)
+    remove_value_from_list(num, x_zero)
     remove_value_from_list(num, x_1)
     remove_value_from_list(num, x2)
-    final_list.append(modif_string_to_int(num))
-    final_list.append(modif_string_to_int(x_1))
-    final_list.append(modif_string_to_int(x2))
+
+    print " After remove num"
+    print num
+    print "x_zero"
+    print x_zero
+    print "x_1"
+    print x_1
+    print "x2"
+    print x2
+    print "\n\n"
+    
+    print "final_list"
+    print final_list
+    final_list.append(modif_string_to_float(num))
+    print "final_list1"
+    print final_list
+    final_list.append(modif_string_to_float(x_zero))
+    print "final_list2"
+    print final_list
+    final_list.append(modif_string_to_float(x_1))
+    print "final_list3"
+    print final_list
+    final_list.append(modif_string_to_float(x2))
+    print "final_list4"
+    print final_list
+   
+    print "num"
+    print num
+    print "x_zero"
+    print x_zero
+    print "x_1"
+    print x_1
+    print "x1"
+    print x1
+    print "x2"
+    print x2
+
     return final_list
+
+def sum_lst_value(lst1, lst2):
+    lst1[0] -= lst2[0]
+    lst1[1] -= lst2[1]
+    lst1[2] -= lst2[2]
+    lst1[3] -= lst2[3]
+    return lst1
+
+def solve_two_solution(liste, discrim):
+    print "value of discrim "+ str(discrim)
+    x1 = (-liste[2] + (discrim**(.5)))/(2*liste[3])
+    x2 = (-liste[2] - (discrim**(.5)))/(2*liste[3])
+    print "Two solutions"
+    print x1
+    print x2
+
+def solve_one_solution(liste, discrim):
+    print "value of discrim "+ str(discrim)
+    x1= -liste[2] / (2 * liste[3])
+    print "Only one solution"
+    print x1
+
+def no_real_root():
+    print "There are no real solutions. The values are complex numbers"
+
+def convert_to_str(value, index):
+    if (value >= 0 and index != 0):
+        tmp = "+"+str(value)
+    else:
+        tmp = str(value)
+    return tmp
+
+def print_reducted_form(liste):
+    index = 0
+    sys.stdout.write("reducted form: ")
+    if liste[0]:
+        sys.stdout.write(convert_to_str(liste[0], index))
+        sys.stdout.write(" ")
+        index += 1
+    if liste[1]:
+        sys.stdout.write(convert_to_str(liste[1], index))
+        sys.stdout.write("X^0 ")
+        index += 1
+    if liste[2]:
+        sys.stdout.write(convert_to_str(liste[2], index))
+        sys.stdout.write("X^1 ")
+        index += 1
+    if liste[3]:
+        sys.stdout.write(convert_to_str(liste[3], index))
+        sys.stdout.write("X^2 ")
+        index += 1
+    sys.stdout.write(" = 0\n")
 
 def test(argv):
     lst = []
-    rigth = []
-    rigth1 = []
-    rigth2 = []
+    discrim = 0
     argv[1] = argv[1].replace(" ", "") 
+    argv[1] = argv[1].replace("*", "") 
+    argv[1] = argv[1].replace("x", "X") 
     lst = argv[1].split('=')
     lst[0] = ' '+lst[0]
     lst[1] = ' '+lst[1]
-
-    list_left = []
-    list_right = []
-    list_left = set_list(lst[0]);
-    list_right = set_list(lst[1]);
-
-
-    print(lst)
-    if list_left:
-        print("display left:")
-        print(list_left)
-    if list_right:
-        print("display right:")
-        print(list_right)
     
+    list_left = parse_value(lst[0])
+    list_right = parse_value(lst[1])
+
+    print "value of liste left right:"
+    print list_left
+    print list_right
+
+    lst = sum_lst_value(list_left, list_right)
+    print_reducted_form(lst)
+
+    if not lst[1]:
+        lst[1] = 0
+    else:
+        lst[1] = 1
+    if not lst[0]:
+        lst[0] = 0
+    lst[1] += lst[0]
+
+
+    if lst[3]:
+        discrim = (lst[2] * lst[2]) - (4 * lst[3] * lst[1])
+    
+    
+    if discrim > 0:
+        solve_two_solution(lst, discrim)
+    elif discrim == 0:
+        solve_one_solution(lst, discrim)
+    else:
+        no_real_root()
 
 if __name__ == "__main__":
     test(sys.argv)
